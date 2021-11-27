db5f02c1b9034a79d62315f9810477211a9894fb
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-11-27 01:22:19 -0500

    Implements hud container view class

--- DLProgressHUD/Classes/DLProgressHUD.swift
+++ DLProgressHUD/Classes/DLProgressHUD.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+public class DLProgressHUD {
+
+    static let shared = DLProgressHUD()
+
+    private var hudContainerView: HudContainerView?
+
+    init() {}
+
+    func show(with configuration: HudConfigurationProtocol = DefaultHudConfiguration.shared,
+              completion: ((Bool) -> Void)? = nil) {
+        // If hud is already being shown we dismiss it.
+        if hudContainerView != nil { dismiss() }
+
+        hudContainerView = HudContainerView(configuration: configuration)
+        guard let hudContainerView = hudContainerView else { return }
+        hudContainerView.alpha = 0.0
+
+        let mainWindow = UIApplication.shared.windows.first ?? UIWindow()
+        mainWindow.addSubview(hudContainerView)
+
+        UIView.animate(withDuration: configuration.presentationAnimationDuration,
+                       delay: 0.0,
+                       options: [.curveEaseOut], animations: {
+            hudContainerView.alpha = 1.0
+        }, completion: { completed in
+            completion?(completed)
+        })
+    }
+
+    func dismiss(with animationDuration: TimeInterval = 0.0) {
+        UIView.animate(withDuration: animationDuration, delay: 0.0, options: [.curveEaseOut], animations: {
+            self.hudContainerView?.alpha = 0.0
+        }, completion: { _ in
+            self.hudContainerView?.removeFromSuperview()
+            self.hudContainerView = nil
+        })
+    }
+
+}
