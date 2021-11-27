5ce820f53793f79125f2dfb4b4e1017750253dd6
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-11-27 01:22:01 -0500

    Implements hud configuration classes

---
+++ DLProgressHUD/Classes/Configuration/HudConfigurationProtocol.swift
@@ -0,0 +1,27 @@
+//
+//  HudConfigurationProtocol.swift
+//  DLProgressHUD
+//
+//  Created by Alonso on 27/11/21.
+//
+
+import UIKit
+
+protocol HudConfigurationProtocol {
+
+    var backgroundColor: UIColor { get }
+    var hudColor: UIColor { get }
+    var activityIndicatorColor: UIColor { get }
+    var activityIndicatorStyle: UIActivityIndicatorView.Style { get }
+
+    var textColor: UIColor { get }
+    var textFont: UIFont { get }
+
+    var hudContentPreferredWidth: CGFloat { get }
+    var hudContentPreferredHeight: CGFloat { get }
+
+    var hudContentCornerRadius: CGFloat { get }
+
+    var presentationAnimationDuration: CGFloat { get }
+
+}
---
+++ DLProgressHUD/Classes/UI/HudContainerView.swift
@@ -0,0 +1,8 @@
+//
+//  HUDContainerView.swift
+//  DLProgressHUD
+//
+//  Created by Alonso on 27/11/21.
+//
+
+import Foundation
