134f89361619a6cc758df1a489b88afd2fdcb303
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-11-28 23:46:16 -0500

    Removes unnecessary code

--- Example/DLProgressHUD/ViewController.swift
+++ Example/DLProgressHUD/ViewController.swift
@@ -11,16 +11,6 @@ import DLProgressHUD
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func showHUDAction(_ sender: Any) {
         DLProgressHUD.show { _ in
             DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
