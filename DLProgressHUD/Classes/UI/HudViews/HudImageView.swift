c1fac181d39c039f86e9dcd211e32613a95c7b03
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-12-11 19:12:30 -0500

    Updates hud image view to support text

--- DLProgressHUD/Classes/UI/HudViews/HudImageView.swift
+++ DLProgressHUD/Classes/UI/HudViews/HudImageView.swift
@@ -9,6 +9,15 @@ import UIKit
 
 final class HudImageView: UIView {
 
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 12
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
     private lazy var imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
@@ -16,16 +25,31 @@ final class HudImageView: UIView {
         return imageView
     }()
 
+    private lazy var textLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = configuration.descriptionTextNumberOfLines
+        label.textAlignment = .center
+        label.font = configuration.descriptionTextFont
+        label.textColor = configuration.descriptionTextColor
+        label.setContentHuggingPriority(.required, for: .vertical)
+        label.setContentCompressionResistancePriority(.required, for: .horizontal)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
     // MARK: - Dependencies
 
     private let configuration: HudConfigurationProtocol
-    private let image: UIImage
+
+    private let hudImage: UIImage
+    private let descriptionText: String?
 
     // MARK: - Initializers
 
-    init(configuration: HudConfigurationProtocol, image: UIImage) {
+    init(configuration: HudConfigurationProtocol, hudImage: UIImage, descriptionText: String? = nil) {
         self.configuration = configuration
-        self.image = image
+        self.hudImage = hudImage
+        self.descriptionText = descriptionText
         super.init(frame: UIScreen.main.bounds)
 
         setupUI()
@@ -38,14 +62,38 @@ final class HudImageView: UIView {
     // MARK: - Private
 
     private func setupUI() {
-        addSubview(imageView)
+        addSubview(stackView)
+        NSLayoutConstraint.activate([stackView.topAnchor.constraint(greaterThanOrEqualTo: topAnchor),
+                                     stackView.bottomAnchor.constraint(lessThanOrEqualTo: bottomAnchor),
+                                     stackView.leadingAnchor.constraint(greaterThanOrEqualTo: leadingAnchor),
+                                     stackView.trailingAnchor.constraint(greaterThanOrEqualTo: trailingAnchor),
+                                     stackView.centerYAnchor.constraint(equalTo: centerYAnchor),
+                                     stackView.centerXAnchor.constraint(equalTo: centerXAnchor)])
+
+        let imageContainterView = UIView()
+        imageContainterView.addSubview(imageView)
         NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: topAnchor, constant: 8),
-            imageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
-            imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
-            imageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8)
+            imageView.topAnchor.constraint(equalTo: imageContainterView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: imageContainterView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: imageContainterView.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: imageContainterView.bottomAnchor),
         ])
-        imageView.image = image
+        imageView.image = hudImage
+
+        stackView.addArrangedSubview(imageContainterView)
+        if let descriptionText = descriptionText {
+            let textLabelContainerView = UIView()
+            textLabelContainerView.addSubview(textLabel)
+            NSLayoutConstraint.activate([
+                textLabel.topAnchor.constraint(equalTo: textLabelContainerView.topAnchor),
+                textLabel.bottomAnchor.constraint(equalTo: textLabelContainerView.bottomAnchor),
+                textLabel.leadingAnchor.constraint(equalTo: textLabelContainerView.leadingAnchor, constant: 8),
+                textLabel.trailingAnchor.constraint(equalTo: textLabelContainerView.trailingAnchor, constant: -8)
+            ])
+
+            stackView.addArrangedSubview(textLabelContainerView)
+            textLabel.text = descriptionText
+        }
     }
 
 }
