fdca3f0bfffb4fd341124f7e85b8948613119550
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-12-14 23:35:42 -0500

    Adds default values in protocol extension

--- DLProgressHUD/Classes/Configuration/HudConfigurationProtocol.swift
+++ DLProgressHUD/Classes/Configuration/HudConfigurationProtocol.swift
@@ -34,3 +34,39 @@ public protocol HudConfigurationProtocol {
     var descriptionTextNumberOfLines: Int { get }
 
 }
+
+public extension HudConfigurationProtocol {
+
+    var backgroundColor: UIColor { .clear }
+
+    var hudColor: UIColor { UIColor(white: 0.8, alpha: 0.36) }
+
+    var activityIndicatorColor: UIColor { UIColor.darkGray }
+
+    var activityIndicatorStyle: UIActivityIndicatorView.Style { .whiteLarge }
+
+    var textColor: UIColor { UIColor.black }
+
+    var textFont: UIFont { UIFont.boldSystemFont(ofSize: 24) }
+
+    var hudContentPreferredWidth: CGFloat { 120.0 }
+
+    var hudContentPreferredHeight: CGFloat { 120.0 }
+
+    var hudContentCornerRadius: CGFloat { 10.0 }
+
+    var hudContentVisualEffectBlurStyle: UIBlurEffect.Style { .light }
+
+    var presentationAnimationDuration: CGFloat { 0.3 }
+
+    var presentationAnimationDelay: CGFloat { 0.0 }
+
+    var backgroundInteractionEnabled: Bool { false }
+
+    var descriptionTextFont: UIFont { .systemFont(ofSize: 16) }
+
+    var descriptionTextColor: UIColor { .black }
+
+    var descriptionTextNumberOfLines: Int { 0 }
+
+}
