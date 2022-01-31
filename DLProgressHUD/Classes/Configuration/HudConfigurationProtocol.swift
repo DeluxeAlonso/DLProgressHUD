b92bd0c989834f197b273633adbd9de12e3c265e
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-01-30 23:18:36 -0500

    Adds docs for HudConfigurationProtocol properties

--- DLProgressHUD/Classes/Configuration/HudConfigurationProtocol.swift
+++ DLProgressHUD/Classes/Configuration/HudConfigurationProtocol.swift
@@ -9,9 +9,14 @@ import UIKit
 
 public protocol HudConfigurationProtocol {
 
+    /// Background color of the view behind the HUD.
     var backgroundColor: UIColor { get }
+    /// HUD container color.
     var hudColor: UIColor { get }
+
+    /// The color of the activity indicator. If you set a color for an activity indicator, it overrides the color provided by the *activityIndicatorStyle* property.
     var activityIndicatorColor: UIColor { get }
+    /// The basic appearance of the activity indicator.
     var activityIndicatorStyle: UIActivityIndicatorView.Style { get }
 
     var hudContentPreferredWidth: CGFloat { get }
@@ -24,6 +29,7 @@ public protocol HudConfigurationProtocol {
     var presentationAnimationDuration: CGFloat { get }
     var presentationAnimationDelay: CGFloat { get }
 
+    /// If set to true, user can interact with the whole screen as long as the HUD is being presented.
     var backgroundInteractionEnabled: Bool { get }
 
     var textFont: UIFont { get }
@@ -33,6 +39,7 @@ public protocol HudConfigurationProtocol {
 
     var hudImageHeight: CGFloat { get }
 
+    /// If set to true HUD gets automatically dismissed after the  elapsed time defined by *presentationDuration* is over.
     var shouldDismissAutomatically: Bool { get }
 
     /// Presentation duration of the progress HUD. It will only have an effect if *shouldDismissAutomatically* is set to true.
