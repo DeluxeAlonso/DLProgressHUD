fa064717eda5c0171e1ab2382780c3da80a1024a
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-02-02 00:49:29 -0500

    Fixes hardcoded text alignment in HudImageView

--- DLProgressHUD/Classes/UI/HudViews/HudImageView.swift
+++ DLProgressHUD/Classes/UI/HudViews/HudImageView.swift
@@ -29,7 +29,7 @@ final class HudImageView: UIView {
     private lazy var textLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = configuration.textNumberOfLines
-        label.textAlignment = .center
+        label.textAlignment = configuration.textAlignment
         label.font = configuration.textFont
         label.textColor = configuration.textColor
         label.setContentHuggingPriority(.required, for: .vertical)
